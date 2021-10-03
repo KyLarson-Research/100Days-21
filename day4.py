b61aa739f7bba687d6ece4d61734867ad2906f9c
KyLarson <68448184+KyLarson-Research@users.noreply.github.com>
2021-10-02 21:15:43 -0700

    Add files via upload

--- day4.py
+++ day4.py
@@ -1,4 +1,8 @@
 #Authored By Kyle Larson 10-2-21
 import random
 computer_choice = random.randrange(3)
-print(computer_choice)
+user_choice = input("Rock, Paper, Scisors")
+if computer_choice == user_choice +1:
+	print("computer wins")
+else:
+	print("user wins")
