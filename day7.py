8fc886262909110fadf48745df23ce763df98f80
Kyle Larson <kylel@email.arizona.edu>
2021-10-08 17:13:30 -0700

    listening to JAZZSTEP https://www.youtube.com/watch?v=1H6XaXdqa54 titled Best JAZZSTEP Drum & Bass Mix 2021 by JR98

--- day7.py
+++ day7.py
@@ -1,4 +1,6 @@
 #Authored by Kyle Larson 10-5-21
+import random as rand
+
 HANGMANPICS = ['''
   +---+
   |   |
@@ -60,3 +62,33 @@ words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
          'wombat zebra ').split()
 
 #The Above Courtesy of https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
+index_of_new_word = rand.randint(0, len(words))
+HANGI = 0
+hangman_wordish = words[index_of_new_word]
+letter_index = 0
+num_correct_letters =0
+while(HANGI<len(HANGMANPICS)):
+    guess = input("Enter guess ")
+    output=[]
+    letter_index=0
+    for letter in hangman_wordish:
+        if letter==guess:
+            output.append(" "+guess+ " ")
+            letter_index = letter_index +1
+            num_correct_letters = num_correct_letters+1
+        else: 
+            output.append(' _ ')
+    if letter_index>0:
+        out =""
+        out = out.join(output)
+        print(out)
+    else:
+        HANGI=HANGI+1
+        print(HANGMANPICS[HANGI])
+        
+    if HANGI == len(HANGMANPICS) or num_correct_letters == len(hangman_wordish):
+        break
+if num_correct_letters == len(hangman_wordish):
+    print("Congradulations!")
+else:
+    print("Game over. You lose.")
